4dfcac37c1d20af432bae8b4ed66246430ed50c6
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-10-19 20:09:09 +0300

    Summary info

--- src/cat-api.js
+++ src/cat-api.js
@@ -7,6 +7,17 @@ function fetchBreeds() {
             return resp.json();
         }
 )};
+
+function fetchCatByBreed(breedId){
+    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(resp => {
+        if (!resp.ok) {
+            throw new Error();
+        }
+        return resp.json();
+    }
+)
+}
      
 
-export { fetchBreeds };
+export { fetchBreeds,  fetchCatByBreed};
--- src/index.js
+++ src/index.js
@@ -1,18 +1,52 @@
-// import axios from "axios";
-import {fetchBreeds} from "./cat-api.js";
+import axios from "axios";
+import {fetchBreeds,  fetchCatByBreed} from "./cat-api.js";
 
-// axios.defaults.headers.common["x-api-key"] = 'live_wGzZUVgEEhQ7o6tbb8g0mGcimVRwBSiqWtl9oI3YdeomRh2q6YRNThds6BXOieSr';
+axios.defaults.headers.common["x-api-key"] = 'live_wGzZUVgEEhQ7o6tbb8g0mGcimVRwBSiqWtl9oI3YdeomRh2q6YRNThds6BXOieSr';
 const selectCat = document.querySelector('.breed-select');  
+const catInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
 
-// fetch('https://api.thecatapi.com/v1/breeds')
-//     .then(resp => {
-//         if (!resp.ok) {
-//             throw new Error();
-//         }
-//         return resp.json();
-// })
-    fetchBreeds().then(arr => {
+loader.classList.add('hidden-part');
+error.classList.add('hidden-part');
+// selectCat.classList.add('hidden-part');
+fetchBreeds().then(arr => {
         const markup = arr.map(({ id, name } = {}) => `<option value="${id}">${name}</option>`).join('');
-        selectCat.insertAdjacentHTML('afterbegin', markup);})
+        selectCat.insertAdjacentHTML('afterbegin', markup);
+        selectCat.addEventListener('change', handleSelect);
+        function handleSelect(){
+            const needBreed = arr.find(item => item.id === selectCat.value);
+            console.log(needBreed);
+            fetchCatByBreed(selectCat.value)
+                .then(resp => {
+                     catInfo.innerHTML = `<img src="${resp[0].url}" alt="cat" width="500">
+                     <h2>${needBreed.name}</h2>
+                     <h3>${needBreed.description}</h3>
+                     <h3>Temperament: ${needBreed.temperament}</h3>`
+                    console.log(resp[0])})
+                .catch(err => console.log(err));}    })
     .catch(err => console.log(err));
     
+      
+
+
+    
+
+        // selectCat.addEventListener('change', handleSelect);
+
+
+        // fetchBreeds().then(arr => {
+        //         arr.forEach(item => arrOfCats.push(item));
+        //         const markup = arr.map(({ id, name } = {}) => `<option value="${id}">${name}</option>`).join('');
+        //         selectCat.insertAdjacentHTML('afterbegin', markup);
+        //         console.log(arrOfCats);        })
+        //     .catch(err => console.log(err));
+            
+            
+        // function handleSelect(){
+        //     fetchCatByBreed(selectCat.value)
+        //         .then(resp => {
+        //              catInfo.innerHTML = `<img src="${resp[0].url}" alt="cat" width="500">
+        //              <h2></h2><h3></h3><h3></h3>`
+        //             console.log(resp[0])})
+        //         .catch(err => console.log(err));}

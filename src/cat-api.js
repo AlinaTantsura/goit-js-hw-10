226a328b9e5f69d01b2898ac536c276b3e56bd3b
AlinaTantsura <alinatanst@gmail.com>
2023-10-20 12:20:43 +0300

    Summary 20_10

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,15 +1,23 @@
+// import axios from "axios";
+// axios.defaults.headers.common["x-api-key"] = 'live_wGzZUVgEEhQ7o6tbb8g0mGcimVRwBSiqWtl9oI3YdeomRh2q6YRNThds6BXOieSr';
+
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY = 'x-api-key=live_wGzZUVgEEhQ7o6tbb8g0mGcimVRwBSiqWtl9oI3YdeomRh2q6YRNThds6BXOieSr';
+
+
 function fetchBreeds() {
-   return fetch('https://api.thecatapi.com/v1/breeds')
+        return fetch(`${BASE_URL}/breeds?${API_KEY}`)
         .then(resp => {
             if (!resp.ok) {
                 throw new Error();
             }
             return resp.json();
-        }
-)};
+        })
+   
+};
 
 function fetchCatByBreed(breedId){
-    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    return fetch(`${BASE_URL}/images/search?${API_KEY}&breed_ids=${breedId}`)
     .then(resp => {
         if (!resp.ok) {
             throw new Error();
--- src/index.js
+++ src/index.js
@@ -1,79 +1,50 @@
-import axios from "axios";
-import {fetchBreeds,  fetchCatByBreed} from "./cat-api.js";
+import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
+import SlimSelect from 'slim-select';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-axios.defaults.headers.common["x-api-key"] = 'live_wGzZUVgEEhQ7o6tbb8g0mGcimVRwBSiqWtl9oI3YdeomRh2q6YRNThds6BXOieSr';
 const selectCat = document.querySelector('.breed-select');  
 const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
 
-loader.classList.add('hidden-part');
-error.classList.add('hidden-part');
-// selectCat.classList.add('hidden-part');
-if(!selectCat.children.length){
-    selectCat.classList.add('hidden-part');
-    loader.classList.remove('hidden-part');
-}
-else{
-    loader.classList.add('hidden-part');
-    selectCat.classList.remove('hidden-part');
-}
+
+selectCat.classList.add('hidden-part');
+// new SlimSelect({
+//             select: "#breed-select",
+// });
+catInfo.classList.add('hidden-part');
+loader.style.display = 'inline-block';
+
 fetchBreeds().then(arr => {
         const markup = arr.map(({ id, name } = {}) => `<option value="${id}">${name}</option>`).join('');
-        selectCat.insertAdjacentHTML('afterbegin', markup);
-        
-        if(!selectCat.children.length){
-            selectCat.classList.add('hidden-part');
-            loader.classList.remove('hidden-part');
-        }
-        else{
-            loader.classList.add('hidden-part');
-            selectCat.classList.remove('hidden-part');
-        }
+    selectCat.insertAdjacentHTML('afterbegin', markup);
+    loader.style.display = 'none';
+    selectCat.classList.remove('hidden-part');
+        new SlimSelect({
+            select: "#breed-select",
+        });
+
         selectCat.addEventListener('change', handleSelect);
-        function handleSelect(){
-            const needBreed = arr.find(item => item.id === selectCat.value);
-            console.log(needBreed);
+    
+            function handleSelect() {
+                catInfo.innerHTML = '';
+                const needBreed = arr.find(item => item.id === selectCat.value);
+                catInfo.classList.add('hidden-part');
+                loader.style.display = 'inline-block';
             fetchCatByBreed(selectCat.value)
                 .then(resp => {
-                     catInfo.innerHTML = `<img src="${resp[0].url}" alt="cat" width="500">
+                     catInfo.innerHTML = `<div class="img-box"><img src="${resp[0].url}" alt="cat" class="cat-img"></div>
+                     <div>
                      <h2>${needBreed.name}</h2>
-                     <h3>${needBreed.description}</h3>
-                     <h3>Temperament: ${needBreed.temperament}</h3>`;
-                     if(!catInfo.children.length){
-                        catInfo.classList.add('hidden-part');
-                        loader.classList.remove('hidden-part');
-                    }
-                    else{
-                        loader.classList.add('hidden-part');
-                        catInfo.classList.remove('hidden-part');
-                    }
-                    console.log(resp[0])})
-                .catch(() => {error.classList.remove('hidden-part');
+                     <p>${needBreed.description}</p>
+                     <p><span class="accent-temp">Temperament:</span> ${needBreed.temperament}</p>
+                     </div>`;
+                    loader.style.display = 'none';
+                    catInfo.classList.remove('hidden-part');
+                })
+                .catch(() => {
+                    Notify.failure('Oops! Something went wrong! Try reloading the page!');
                             loader.classList.add('hidden-part')});}    })
-    .catch(() => {error.classList.remove('hidden-part');
-    loader.classList.add('hidden-part')});
-    
-      
-
-
-    
-
-        // selectCat.addEventListener('change', handleSelect);
-
-
-        // fetchBreeds().then(arr => {
-        //         arr.forEach(item => arrOfCats.push(item));
-        //         const markup = arr.map(({ id, name } = {}) => `<option value="${id}">${name}</option>`).join('');
-        //         selectCat.insertAdjacentHTML('afterbegin', markup);
-        //         console.log(arrOfCats);        })
-        //     .catch(err => console.log(err));
-            
-            
-        // function handleSelect(){
-        //     fetchCatByBreed(selectCat.value)
-        //         .then(resp => {
-        //              catInfo.innerHTML = `<img src="${resp[0].url}" alt="cat" width="500">
-        //              <h2></h2><h3></h3><h3></h3>`
-        //             console.log(resp[0])})
-        //         .catch(err => console.log(err));}
+    .catch(() => {Notify.failure('Oops! Something went wrong! Try reloading the page!');
+        loader.classList.add('hidden-part')
+    }
+    );

0aff2634e37cf26b5aba28db8dfaab5afbb23333
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-10-21 09:58:50 +0300

    Summary

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,31 +1,39 @@
-// import axios from "axios";
-// axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
-// axios.defaults.headers.common["x-api-key"] = 'live_wGzZUVgEEhQ7o6tbb8g0mGcimVRwBSiqWtl9oI3YdeomRh2q6YRNThds6BXOieSr';
-
-const BASE_URL = 'https://api.thecatapi.com/v1';
-const API_KEY = 'x-api-key=live_wGzZUVgEEhQ7o6tbb8g0mGcimVRwBSiqWtl9oI3YdeomRh2q6YRNThds6BXOieSr';
+// For Axios-requests
+import axios from "axios";
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
+axios.defaults.headers.common["x-api-key"] = 'live_wGzZUVgEEhQ7o6tbb8g0mGcimVRwBSiqWtl9oI3YdeomRh2q6YRNThds6BXOieSr';
 
 function fetchBreeds() {
-    return fetch(`${BASE_URL}/breeds?${API_KEY}`)
-    .then(resp => {
-        if (!resp.ok) {
-            throw new Error();
-        }
-        return resp.json();
-    })
-
+    return axios.get('/breeds');
 };
 
 function fetchCatByBreed(breedId){
-    return fetch(`${BASE_URL}/images/search?${API_KEY}&breed_ids=${breedId}`)
-    .then(resp => {
-        if (!resp.ok) {
-            throw new Error();
-        }
-        return resp.json();
-    }
-)
-}
-     
+    return axios.get(`/images/search?breed_ids=${breedId}`)}
+
+
+    //For fetch-requests
+// const BASE_URL = 'https://api.thecatapi.com/v1';
+// const API_KEY = 'x-api-key=live_wGzZUVgEEhQ7o6tbb8g0mGcimVRwBSiqWtl9oI3YdeomRh2q6YRNThds6BXOieSr';
+//
+// function fetchBreeds() {
+//     return fetch(`${BASE_URL}/breeds?${API_KEY}`)
+//     .then(resp => {
+//         if (!resp.ok) {
+//             throw new Error();
+//         }
+//         return resp.json();
+//     })
+
+// };
+// function fetchCatByBreed(breedId){
+//     return fetch(`${BASE_URL}/images/search?${API_KEY}&breed_ids=${breedId}`)
+//     .then(resp => {
+//         if (!resp.ok) {
+//             throw new Error();
+//         }
+//         return resp.json();
+//     }
+// )
+// }
 
 export { fetchBreeds,  fetchCatByBreed};
--- src/index.js
+++ src/index.js
@@ -4,31 +4,34 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const selectCat = document.querySelector('.breed-select');  
 const catInfo = document.querySelector('.cat-info');
+const loaderText = document.querySelector('.loader-text');
 const loader = document.querySelector('.loader');
 
 catInfo.classList.add('hidden-part');
-loader.classList.remove('hidden-part');
+loaderText.classList.remove('hidden-part');
 
-fetchBreeds().then(arr => {
-        const markup = arr.map(({ id, name } = {}) => `<option value="${id}">${name}</option>`).join('');
+
+// For Axios-requests
+fetchBreeds().then(obj => {
+        const markup = obj.data.map(({ id, name } = {}) => `<option value="${id}">${name}</option>`).join('');
     selectCat.insertAdjacentHTML('afterbegin', markup);
-    loader.classList.add('hidden-part');
+    loaderText.classList.add('hidden-part');
 
     new SlimSelect({
         select: "#breed-select",
     });
-    // selectCat.classList.remove('hidden-part');
+    selectCat.classList.remove('hidden-part');
     selectCat.addEventListener('change', handleSelect);
     
     function handleSelect() {
         catInfo.innerHTML = '';
-        const needBreed = arr.find(item => item.id === selectCat.value);
+        const needBreed = obj.data.find(item => item.id === selectCat.value);
         catInfo.classList.add('hidden-part');
         loader.classList.remove('hidden-part');
         
         fetchCatByBreed(selectCat.value)
             .then(resp => {
-                catInfo.innerHTML = `<div class="img-box"><img src="${resp[0].url}" alt="cat" class="cat-img"></div>
+                catInfo.innerHTML = `<div class="img-box"><img src="${resp.data[0].url}" alt="cat" class="cat-img"></div>
                 <div>
                 <h2>${needBreed.name}</h2>
                 <p>${needBreed.description}</p>
@@ -39,8 +42,47 @@ fetchBreeds().then(arr => {
                 })
             .catch(() => {
                 Notify.failure('Oops! Something went wrong! Try reloading the page!');
-                loader.classList.add('hidden-part');;});
+                loaderText.classList.add('hidden-part');
+                loader.classList.add('hidden-part');});
             }    
 }).catch(() => {Notify.failure('Oops! Something went wrong! Try reloading the page!');
-    loader.classList.add('hidden-part');;
+    loaderText.classList.add('hidden-part');
+    loader.classList.add('hidden-part');
     });
+
+    //For fetch-requests!!!
+//     fetchBreeds().then(arr => {
+//         const markup = arr.map(({ id, name } = {}) => `<option value="${id}">${name}</option>`).join('');
+//     selectCat.insertAdjacentHTML('afterbegin', markup);
+//     loader_text.classList.add('hidden-part');
+
+//     new SlimSelect({
+//         select: "#breed-select",
+//     });
+//     selectCat.classList.remove('hidden-part');
+//     selectCat.addEventListener('change', handleSelect);
+    
+//     function handleSelect() {
+//         catInfo.innerHTML = '';
+//         const needBreed = arr.find(item => item.id === selectCat.value);
+//         catInfo.classList.add('hidden-part');
+//         loader.classList.remove('hidden-part');
+        
+//         fetchCatByBreed(selectCat.value)
+//             .then(resp => {
+//                 catInfo.innerHTML = `<div class="img-box"><img src="${resp[0].url}" alt="cat" class="cat-img"></div>
+//                 <div>
+//                 <h2>${needBreed.name}</h2>
+//                 <p>${needBreed.description}</p>
+//                 <p><span class="accent-temp">Temperament:</span> ${needBreed.temperament}</p>
+//                 </div>`;
+//                 loader.classList.add('hidden-part');
+//                 catInfo.classList.remove('hidden-part');
+//                 })
+//             .catch(() => {
+//                 Notify.failure('Oops! Something went wrong! Try reloading the page!');
+//                 loader.classList.add('hidden-part');});
+//             }    
+// }).catch(() => {Notify.failure('Oops! Something went wrong! Try reloading the page!');
+//     loader.classList.add('hidden-part');
+//     });
